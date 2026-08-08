f5e5315945cc7685ed4f2cff78dfa1f9f1556a94
priteekadam024-gif <priteekadam024@gmail.com>
2026-08-08 18:43:53 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ const inMemoryMediaFiles: any[] = [];
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 10000;
 
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
